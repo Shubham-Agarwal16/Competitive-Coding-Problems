86d7ec0cf556fa1f2d44c61996bb0889ce2f0cc8
Shubham Agarwal <16shubhamagarwal@gmail.com>
2020-01-26 00:36:47 +0530

    code

---
+++ balance bracket.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<stack>
+#include<cstring>
+
+using namespace std;
+
+bool isOpen( char c ){
+    if(c == '(' || c == '{' || c == '[')
+        return true;
+    return false;
+}
+
+bool isBalanced(char popped, char now ){
+    if(popped == '(' && now ==')')
+        return true;
+    if(popped == '[' && now ==']')
+        return true;
+    if(popped == '{' && now =='}')
+        return true;
+    return false;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while( t-- ){
+        stack<char> s;
+        string str;
+        cin >> str;
+        int flag = 0;
+        for( int i=0; i<str.length(); i++ ){
+            char now = str[i];
+            if( isOpen(now) ) s.push(now);
+            else{
+                char popped;
+                if( s.size()>0 ){
+                    popped = s.top();
+                    s.pop();
+                }
+                else{
+                    flag=1;
+                    break;
+                }
+                if( isBalanced(popped, now)) continue;
+                else
+                {
+                    flag=1;
+                    break;
+                }
+                
+            }
+        }
+        if(s.size() != 0)
+            flag = 1;
+        if(flag)
+            cout<<"NO\n";
+        else
+        {
+            cout<<"YES\n";
+        }
+    }
+    return 0;
+}
